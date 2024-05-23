11356cb21d1084adf4bdab22c23f0e13d7faee06
Eranda Hasaranga <ehasaranga@gmail.com>
2024-05-23 08:51:34 +0530

    defineModel readonly

--- src/core/defineModel.ts
+++ src/core/defineModel.ts
@@ -1,6 +1,6 @@
 import { endpoint } from "@core";
 
-export const defineModel = <N extends string,E, K, T extends TDefineModel<N, E, K> = TDefineModel<N, E, K>>(args: TDefineModel<N, E, K>): T => {
+export const defineModel = <N extends string,E, K, T extends TDefineModel<N, E, K> = TDefineModel<N, E, K>>(args: TDefineModel<N, E, K>) => {
 
     // const crud = typeof args.crud === 'undefined' ? true : args.crud;
 
@@ -11,7 +11,7 @@ export const defineModel = <N extends string,E, K, T extends TDefineModel<N, E, 
         entity: args.entity,
         endpoints,
         repo: args.repo
-    } as T
+    } as const
 
 }
 
