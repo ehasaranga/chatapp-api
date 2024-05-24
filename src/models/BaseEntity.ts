525ee1b852bfa53366329bc6ad5ee171671b386d
Eranda Hasaranga <ehasaranga@gmail.com>
2024-05-24 11:00:43 +0530

    base entity abstract

--- src/models/BaseEntity.ts
+++ src/models/BaseEntity.ts
@@ -1,6 +1,7 @@
-import { PrimaryKey, Property, SerializedPrimaryKey } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property, SerializedPrimaryKey } from '@mikro-orm/core';
 import { ObjectId } from '@mikro-orm/mongodb';
 
+@Entity({ abstract: true })
 export abstract class BaseEntity {
 
   @PrimaryKey()
