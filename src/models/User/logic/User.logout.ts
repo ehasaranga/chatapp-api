8697cc9e46d7bd51c6085bc086b34973378512eb
Eranda Hasaranga <ehasaranga@gmail.com>
2024-06-18 09:46:57 +0530

    cleanup

--- src/models/User/logic/User.logout.ts
+++ src/models/User/logic/User.logout.ts
@@ -1,10 +1,5 @@
 
-import { User } from "@app/models/User/User";
 import { endpoint, validate } from "@core";
-import z from "zod";
-import bcrypt from "bcryptjs";
-import jwt from 'jsonwebtoken'
-import ms from "ms";
 
 export const UserLogin = endpoint({
     path: '/logout',
